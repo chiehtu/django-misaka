4f80f6c09fdc3aa208a68aaa9cd425f0e261ad95
Chieh Tu <me@chiehtu.net>
2017-03-11 20:54:31 +0800

    refactor: update code to work with Misaka v2.1

--- django_misaka/templatetags/markdown.py
+++ django_misaka/templatetags/markdown.py
@@ -11,8 +11,8 @@ from pygments.formatters import HtmlFormatter
 register = template.Library()
 
 
-class MisakaRenderer(m.HtmlRenderer, m.SmartyPants):
-    def block_code(self, text, lang):
+class MisakaRenderer(m.HtmlRenderer):
+    def blockcode(self, text, lang):
         if not lang:
             return '\n<pre><code>%s</code></pre>\n' % \
                 h.escape_html(text.strip())
@@ -27,7 +27,7 @@ def markdown_filter(value):
     renderer = MisakaRenderer(flags=m.HTML_ESCAPE)
     md = m.Markdown(renderer,
                     extensions=m.EXT_FENCED_CODE | m.EXT_NO_INTRA_EMPHASIS)
-    return md.render(value)
+    return md(value)
 
 
 @register.tag(name='markdown')
@@ -49,5 +49,4 @@ class MisakaNode(template.Node):
         renderer = MisakaRenderer(flags=m.HTML_ESCAPE)
         md = m.Markdown(renderer,
                         extensions=m.EXT_FENCED_CODE | m.EXT_NO_INTRA_EMPHASIS)
-        return md.render(value)
-
+        return md(value)
