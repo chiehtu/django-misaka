18cc68830be20f5a22bae31b5b96e88355942b79
Chieh Tu <me@chiehtu.net>
2017-03-12 16:57:29 +0800

    chore: bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-misaka',
-    version='0.2.0',
+    version='0.2.1',
     packages=['django_misaka'],
     include_package_data=True,
     install_requires=[
@@ -31,10 +31,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
