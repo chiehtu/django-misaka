1267f41c35007831b2badef1d9870ef6fd2631ed
Chieh Tu <chiehtu@users.noreply.github.com>
2023-05-05 12:51:32 +0000

    build: update Pygments to 2.15.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     python_requires='~=3.7',
     install_requires=[
         'misaka~=2.1.1',
-        'Pygments~=2.12.0',
+        'Pygments~=2.15.1',
     ],
     license='MIT',
     description=('A Django template tag for rendering '
